9d8d7f910408935d7423d6637f38c3d773ad6b76
Yana Sozonova <ynakochneva@mail.ru>
2025-02-20 12:48:35 +0500

    реализация CRUD для курса через Viewsets, настройка маршрутизации для нее

--- materials/urls.py
+++ materials/urls.py
@@ -1,8 +1,14 @@
 from materials.apps import MaterialsConfig
+from rest_framework.routers import DefaultRouter
+
+from materials.views import CourseViewSet
 
 
 app_name = MaterialsConfig.name
 
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='course')
+
 urlpatterns = [
 
-]
+] + router.urls
--- materials/views.py
+++ materials/views.py
@@ -1,1 +1,9 @@
 from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
+
+from rest_framework.viewsets import ViewSet
+
+
+class CourseViewSet(ViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
