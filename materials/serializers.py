664ce9a630cbfbe76913f0637adbe90b58465f6c
Yana Sozonova <ynakochneva@mail.ru>
2025-02-23 23:53:21 +0500

    добавлен в сериализатор поле вывода количества уроков и поле вывода уроков

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,14 +3,20 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
+        fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    lessons = LessonSerializer(many=True, read_only=True)
+    count_lessons_in_course = serializers.SerializerMethodField()
+
+    def get_count_lessons_in_course(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+    class Meta:
+        model = Course
         fields = "__all__"
