9d620ed4fd67a58d8bd202eb4d33ef98ed1b4770
Yana Sozonova <ynakochneva@mail.ru>
2025-04-12 12:32:19 +0500

    изменения в settings.py

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/media/"
