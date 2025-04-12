c762eaa05a3941873980726183033a93a2ff4b60
Yana Sozonova <ynakochneva@mail.ru>
2025-04-12 17:30:09 +0500

    изменения в settings.py

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,7 @@ SIMPLE_JWT = {
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
